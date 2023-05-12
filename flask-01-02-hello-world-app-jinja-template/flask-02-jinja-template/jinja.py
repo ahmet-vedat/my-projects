2b0d2b91d08c6bd63b626cf9d8323a822aa6c27c
ahmet-vedat <130449661+ahmet-vedat@users.noreply.github.com>
2023-05-12 15:48:47 +0300

    Update jinja.py

--- flask-01-02-hello-world-app-jinja-template/flask-02-jinja-template/jinja.py
+++ flask-01-02-hello-world-app-jinja-template/flask-02-jinja-template/jinja.py
@@ -9,4 +9,5 @@ def number():
     y=20
     return render_template('body.html', num1=x, num2=y, mult=x*y)
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    app.run(host= '0.0.0.0', port=80)
